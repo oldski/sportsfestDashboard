b703aca2a92322e707b9762b677481dc3dc0d3fb
kristopher olds <oldski@users.noreply.github.com>
2025-10-08 17:05:39 -0400

    fixed bug with og image

--- apps/marketing/app/og-image/route.tsx
+++ apps/marketing/app/og-image/route.tsx
@@ -5,8 +5,8 @@ import { OgImage } from '@workspace/ui/components/og-image';
 export const runtime = 'edge';
 
 export async function GET() {
-  const logoUrl = 'http://localhost:3001/assets/logo-sportsfest-full-white.png';
-  const backgroundUrl = 'http://localhost:3001/assets/og-image-background.jpg';
+  const logoUrl = 'https://join.sportsfest.com/assets/logo-sportsfest-full-white.png';
+  const backgroundUrl = 'https://join.sportsfest.com/assets/og-image-background.jpg';
 
   return new ImageResponse(<OgImage logoSrc={logoUrl} backgroundSrc={backgroundUrl} />, {
     width: 1200,
